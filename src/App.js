71083ca1488a4058a82d6fd09c2e025ca5d63371
Rowan <Olsonrowan@gmail.com>
2020-10-07 00:13:37 -0600

    Added Firebase Config

--- src/App.js
+++ src/App.js
@@ -1,1 +1,9 @@
 import React from 'react';
+import { render } from 'react-dom';
+
+function App(){
+    render(
+        <h1>firebase log in</h1>
+    )
+}
+export default App
--- src/services/firebase.js
+++ src/services/firebase.js
@@ -1,8 +1,15 @@
 import firebase from 'firebase'
 import { FIREBASE } from '../keys'
 ​
-const firebaseConfig = FIREBASE
-​
+const  firebaseConfig = {
+    apiKey: "AIzaSyCqv0hagIZ7blvFyAKNt3lGtam_Ew_70mM",
+    authDomain: "slackclone-41796.firebaseapp.com",
+    databaseURL: "https://slackclone-41796.firebaseio.com",
+    projectId: "slackclone-41796",
+    storageBucket: "slackclone-41796.appspot.com",
+    messagingSenderId: "190003322978",
+    appId: "1:190003322978:web:5d4aeb05c93eb00fa8313b"
+  };
 firebase.initializeApp(firebaseConfig)
 ​
 export const auth = firebase.auth
